7d27288fa28195ede58a1848617eb5736347c60b
Yana Kultysheva <ykultysheva@gmail.com>
2016-01-29 18:10:59 -0500

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :name, presence: true 
+
   has_many :reviews
-  has_many :products, through: :reviews 
+  has_many :products, through: :reviews
 end
